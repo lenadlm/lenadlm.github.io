fffeff73246fafe14210c267150615557d8e5627
Leonard Mbelle <58833407+lenadlm@users.noreply.github.com>
2023-02-19 23:24:19 +0300

    Lenadlm patch 2.2 (#24)
    * Update index.html
    
    update meta tags - opengraph
    
    * Create script.js
    
    * Update index.html
    
    * Update script.js
    
    * Update styles.css
    
    * Update index.html
    
    * Update index.html
    
    * Update script.js
    
    * Update script.js
    
    * Update styles.css
    
    * Update index.html
    
    * Update styles.css
    
    * Update styles.css

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
+const timeElement = document.querySelector('.time');
+
 function updateTime() {
-  const date = new Date();
-  const options = {
-    timeZone: 'Africa/Nairobi',
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric',
-    weekday: 'short',
-    day: 'numeric',
-    month: 'long'
-  };
-  const timeString = date.toLocaleString('en-US', options);
-  const navbarElement = document.getElementById('navbar-time');
-  navbarElement.innerHTML = timeString;
+  const now = new Date();
+  const options = { hour: 'numeric', minute: 'numeric', hour12: true };
+  const shortTime = now.toLocaleTimeString('en-US', options);
+  timeElement.innerHTML = shortTime;
 }
 
-setInterval(updateTime, 1000);
+function showLongTime() {
+  const now = new Date();
+  const options = { weekday: 'short', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true };
+  const longTime = now.toLocaleDateString('en-US', options);
+  timeElement.classList.add('time-long');
+  timeElement.textContent = longTime;
+}
+
+function hideLongTime() {
+  const now = new Date();
+  const options = { hour: 'numeric', minute: 'numeric', hour12: true };
+  const shortTime = now.toLocaleTimeString('en-US', options);
+  timeElement.classList.remove('time-long');
+  timeElement.innerHTML = shortTime;
+}
+
+// update the time every 5 seconds
+setInterval(updateTime, 5000);
+
+// add event listeners to show/hide long time
+timeElement.addEventListener('mouseenter', showLongTime);
+timeElement.addEventListener('mouseleave', hideLongTime);
